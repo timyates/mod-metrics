162199663e7e0df571c6fe86a2c9c3a07a64236b
Marc-André Weber <marcandre.weber@post.ch>
2016-01-29 13:52:06 +0100

    Adopted all Groovy-UnitTests to Java-UnitTests

--- src/test/java/com/bloidonia/metrics/integration/MetricsTests.java
+++ src/test/java/com/bloidonia/metrics/integration/MetricsTests.java
@@ -24,7 +24,6 @@ import org.junit.runner.RunWith;
 
 /**
  * Class MetricsTests.
- * This represents TODO.
  *
  * @author webermarca
  * @version $$Revision$$
@@ -37,6 +36,10 @@ public class MetricsTests {
     Logger log = LoggerFactory.getLogger(MetricsTests.class);
     String address = "com.bloidonia.metrics";
 
+    private final String COUNTER = "test.counter";
+    private final String GAUGE = "test.gauge";
+    private final String OK = "ok";
+
     @Before
     public void setUp(TestContext context) {
         vertx = Vertx.vertx();
@@ -46,21 +49,138 @@ public class MetricsTests {
     @Test
     public void testCounterInc(TestContext context){
         Async async = context.async();
-        eventBusSend(buildOperation("test.counter", "inc"), event -> {
-            eventBusSend(buildOperation("counters"), event1 -> {
-                context.assertEquals("ok", event1.result().body().getString("status"));
-                context.assertEquals(1, event1.result().body().getJsonObject("test.counter").getInteger("count"));
-                eventBusSend(buildOperation("test.counter", "dec"), event2 -> {
+        eventBusSend(incOperation(COUNTER), event -> {
+            eventBusSend(countersOperation(), event1 -> {
+                context.assertEquals(OK, extractStatus(event1));
+                context.assertEquals(1, event1.result().body().getJsonObject(COUNTER).getInteger("count"));
+
+                // Reset to 0
+                eventBusSend(decOperation(COUNTER), event2 -> {
                     async.complete();
                 });
             });
         });
     }
 
+    @Test
+    public void testCounterInc4(TestContext context){
+        Async async = context.async();
+        eventBusSend(incOperation(COUNTER, 4), event -> {
+           eventBusSend(countersOperation(), reply -> {
+               context.assertEquals(OK, extractStatus(reply));
+               context.assertEquals(4, reply.result().body().getJsonObject(COUNTER).getInteger("count"));
+
+               // Reset to 0
+               eventBusSend(decOperation(COUNTER, 4), event2 -> {
+                   async.complete();
+               });
+           });
+        });
+    }
+
+    @Test
+    public void testCounterDec(TestContext context) {
+        Async async = context.async();
+        eventBusSend(decOperation(COUNTER), event -> {
+            eventBusSend(countersOperation(), reply -> {
+                context.assertEquals(OK, extractStatus(reply));
+                context.assertEquals(-1, reply.result().body().getJsonObject(COUNTER).getInteger("count"));
+
+                // Reset to 0
+                eventBusSend(incOperation(COUNTER), event1 -> {
+                    async.complete();
+                });
+            });
+        });
+    }
+
+    @Test
+    public void testCounterDec4(TestContext context) {
+        Async async = context.async();
+        eventBusSend(decOperation(COUNTER, 4), event -> {
+            eventBusSend(countersOperation(), reply -> {
+                context.assertEquals(OK, extractStatus(reply));
+                context.assertEquals(-4, reply.result().body().getJsonObject(COUNTER).getInteger("count"));
+
+                // Reset to 0
+                eventBusSend(incOperation(COUNTER, 4), event1 -> {
+                    async.complete();
+                });
+            });
+        });
+    }
+
+    @Test
+    public void testGauges(TestContext context) {
+        Async async = context.async();
+        eventBusSend(setOperation(GAUGE, 1234), event -> {
+            eventBusSend(gaugesOperation(), reply -> {
+                context.assertEquals(OK, extractStatus(reply));
+                context.assertEquals(1234, reply.result().body().getJsonObject(GAUGE).getInteger("value"));
+
+                eventBusSend(setOperation(GAUGE, 5678), event1 -> {
+                    eventBusSend(gaugesOperation(), reply2 -> {
+                        context.assertEquals(OK, extractStatus(reply2));
+                        context.assertEquals(5678, reply2.result().body().getJsonObject(GAUGE).getInteger("value"));
+
+                        eventBusSend(removeOperation(GAUGE), event2 -> {
+                            async.complete();
+                        });
+                    });
+                });
+            });
+        });
+    }
+
+    private String extractStatus(AsyncResult<Message<JsonObject>> reply){
+        return reply.result().body().getString("status");
+    }
+
+    private JsonObject countersOperation(){
+        return buildOperation("counters");
+    }
+
+    private JsonObject gaugesOperation(){
+        return buildOperation("gauges");
+    }
+
+    private JsonObject incOperation(String name){
+        return buildOperation(name, "inc");
+    }
+
+    private JsonObject incOperation(String name, int n){
+        JsonObject op = incOperation(name);
+        op.put("n", n);
+        return op;
+    }
+
+    private JsonObject removeOperation(String name){
+        return buildOperation(name, "remove");
+    }
+
+    private JsonObject setOperation(String name){
+        return buildOperation(name, "set");
+    }
+
+    private JsonObject setOperation(String name, int n){
+        JsonObject op = setOperation(name);
+        op.put("n", n);
+        return op;
+    }
+
+    private JsonObject decOperation(String name){
+        return buildOperation(name, "dec");
+    }
+
+    private JsonObject decOperation(String name, int n){
+        JsonObject op = decOperation(name);
+        op.put("n", n);
+        return op;
+    }
+
     private JsonObject buildOperation(String name, String action){
-        JsonObject op = new JsonObject();
+        JsonObject op = buildOperation(action);
         op.put("name", name);
-        op.put("action", action);
         return op;
     }
 
